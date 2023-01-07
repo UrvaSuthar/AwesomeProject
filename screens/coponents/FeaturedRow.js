150176ae15c3de9a13f71646ab54c65e83ccb86d
shatel7448@gmail.com <urvasuthar2@gmail.com>
2023-01-07 20:33:38 +0530

    restaurant card

--- screens/coponents/FeaturedRow.js
+++ screens/coponents/FeaturedRow.js
@@ -3,23 +3,36 @@ import React from 'react'
 import {
   ArrowRightIcon
 } from "react-native-heroicons/outline";
+import RestaurantCard from './RestaurantCard.js'
 const FeaturedRow = ({ id, title, description }) => {
-  
 
   return (
     <View>
       <View className='flex-row mt-4 items-center justify-between px-4'>
-        <Text className = 'text-lg font-bold'>{title}</Text>
-        <ArrowRightIcon size={25}/>
+        <Text className='text-lg font-bold'>{title}</Text>
+        <ArrowRightIcon size={25} color='#DC0000'/>
       </View>
-      <Text className = 'text-xs text-gray-500 px-4' >{description}</Text>
+      <Text className='text-xs text-gray-500 px-4' >{description}</Text>
       <ScrollView
-      horizontal
-      contentContainerStyle={{
-        paddingHorizontal :15,
-      }}      
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className='pt-4'
       >
-
+        {/* Restaurant Cards */}
+        <RestaurantCard
+        id=''
+        imgUrl=''
+        title=''
+        rating=''
+        gener=''
+        address=''
+        short_description=''
+        dishes=''
+        long=''
+        lat=''/>
       </ScrollView>
     </View>
   );
--- screens/coponents/RestaurantCard.js
+++ screens/coponents/RestaurantCard.js
@@ -2,32 +2,24 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { Card } from 'react-native-elements';
 
-const RestaurantCard = ({ name, image, description }) => {
+const RestaurantCard = ({ 
+  id,
+  imgUrl,
+  title,
+  rating,
+  gener,
+  address,
+  short_description,
+  dishes,
+  long,
+  lat
+ }) => {
   return (
     <TouchableOpacity
       activeOpacity={0.8}
       style={{ width: '32%', margin: 8 }}
     >
-      <Card
-        title={name}
-        image={{ uri: image }}
-        containerStyle={{
-          borderRadius: 8,
-          overflow: 'hidden',
-        }}
-        titleStyle={{
-          fontSize: 18,
-          fontWeight: 'bold',
-          color: '#333',
-        }}
-        imageStyle={{
-          height: 120,
-          borderTopLeftRadius: 8,
-          borderTopRightRadius: 8,
-        }}
-      >
-        <Text className="text-gray-600 text-sm">{description}</Text>
-      </Card>
+      
     </TouchableOpacity>
   );
 };
